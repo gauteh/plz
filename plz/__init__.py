d01aa952affbff9287ba8632a42a75c812f7c8cb
Gaute Hope <eg@gaute.vetsj.com>
2024-02-19 11:15:32 +0100

    log init

--- plz/__init__.py
+++ plz/__init__.py
@@ -1,2 +1,12 @@
+import logging
+
 from . import map
 from . import xr
+
+
+def log(level='debug', mpl_level=logging.WARNING):
+    import coloredlogs
+    coloredlogs.install(level)
+
+    import logging
+    logging.getLogger('matplotlib').setLevel(mpl_level)
