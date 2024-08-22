7131f1d2f2cdfecaa5d246d92891ae7b7e74a200
Gaute Hope <eg@gaute.vetsj.com>
2024-08-22 14:59:12 +0200

    add closest_point

--- plz/xr.py
+++ plz/xr.py
@@ -1,9 +1,11 @@
 import xarray as xr
 import pandas as pd
+import numpy as np
 import logging
 from datetime import timedelta
 
 logger = logging.getLogger(__name__)
+
 
 def open_mfdataset_overlap(url_base, time_series, timedim='time'):
     """
@@ -20,15 +22,26 @@ def open_mfdataset_overlap(url_base, time_series, timedim='time'):
     urls = [t.strftime(url_base) for t in time_series]
     time_step = time_series[1] - time_series[0]
     logger.debug('Opening individual URLs...')
-    datasets = [xr.open_dataset(u, chunks='auto').sel({timedim: slice(t, t+time_step-timedelta(seconds=1))})
-                for u,t in zip(urls, time_series)]
+    datasets = [
+        xr.open_dataset(u, chunks='auto').sel(
+            {timedim: slice(t, t + time_step - timedelta(seconds=1))})
+        for u, t in zip(urls, time_series)
+    ]
     logger.debug('Concatenating...')
-    ds = xr.concat(datasets, dim=timedim,
-                   compat='override', combine_attrs='override', join='override', coords='all')
+    ds = xr.concat(datasets,
+                   dim=timedim,
+                   compat='override',
+                   combine_attrs='override',
+                   join='override',
+                   coords='all')
     return ds
 
 
-def reset_time(ds: xr.Dataset, dim: str = 'time', unit = 's', ref: pd.Timestamp = None, preserve_units = False):
+def reset_time(ds: xr.Dataset,
+               dim: str = 'time',
+               unit='s',
+               ref: pd.Timestamp = None,
+               preserve_units=False):
     """
     Reset time to relative time in seconds since start (without units).
 
@@ -48,12 +61,13 @@ def reset_time(ds: xr.Dataset, dim: str = 'time', unit = 's', ref: pd.Timestamp 
     if not preserve_units:
         t = t / pd.to_timedelta(1., unit)
 
-    ds = ds.assign_coords({ dim : t })
+    ds = ds.assign_coords({dim: t})
 
     if not preserve_units:
         ds[dim].attrs['unit'] = 's'
 
     return ds
+
 
 def nc_cmp(ds: xr.Dataset):
     """
@@ -64,3 +78,35 @@ def nc_cmp(ds: xr.Dataset):
         encoding[v] = {'zlib': True}
 
     return encoding
+
+
+def closest_point(ds, x, y):
+    """
+    Find the index of the closest points in `ds` of `x` and `y`, when `x` and
+    `y` are not dimensions but variables mapped on the coordinate variables.
+    E.g. `lat` and `lon` 2D variables on `X` and `Y` 1D projection variables.
+
+    e.g.:
+
+    >>> lat, lon = 60.372601, 5.166712
+    >>>
+    >>> ds = xr.open_dataset('https://thredds.met.no/thredds/dodsC/sea/norkyst800m/1h/aggregate_be')
+    >>> print(ds)
+    >>>
+    >>> # find closest point in lat, lon grid
+    >>> ds = plz.xr.closest_point(ds, xr.DataArray([lat], dims='lat'), xr.DataArray([lon], dims='lon'))
+    >>> print(ds)
+    """
+
+    dist_x = ds[x.dims[0]].values - x.values
+    dist_y = ds[y.dims[0]].values - y.values
+    dist = dist_x**2 + dist_y**2 # n2 norm
+
+    xi, yi = np.unravel_index(np.argmin(dist), dist.shape)
+
+    assert ds[x.dims[0]].dims == ds[y.dims[0]].dims
+
+    xdim = ds[x.dims[0]].dims[0]
+    ydim = ds[x.dims[0]].dims[1]
+
+    return ds.isel({ xdim : xi, ydim : yi })
